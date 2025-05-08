1edc725e173f3dcdff772f617e62bf31b087fe59
Albert Ye <aly@ocf.berkeley.edu>
2025-05-07 21:51:55 -0700

    feat: run n=5000

--- client/main.sh
+++ client/main.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Array of num_threads values
-num_threads_values=(10 5 2)
+num_threads_values=(200 150 125 100 75 50 20 10 5 2)
 
 # Loop through each value of num_threads
 for num_threads in "${num_threads_values[@]}"
 do
     # Run the command with current num_threads
     echo "Number of threads: $num_threads"
-    python3 benchmark.py 10 $num_threads false
+    python3 benchmark.py --runs 10 10 $num_threads true
 done
